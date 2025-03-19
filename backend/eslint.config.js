91809b75181d30e322f7eff7170fd2e1242f4e5c
ValRoss132 <valross1321@gmail.com>
2025-03-19 13:26:52 +0300

    eslint + prettier to back

---
+++ backend/eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+    { ignores: ['dist', 'node_modules'] },
+    {
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            prettierConfig,
+        ],
+        files: ['**/*.ts'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+        },
+        plugins: {
+            prettier,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                { argsIgnorePattern: '^_' },
+            ],
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            'no-console': 'warn',
+        },
+    },
+);
