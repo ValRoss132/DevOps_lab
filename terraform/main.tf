68c39b059bf6bcbf4ae35048729f54fa4b447bb0
ValRoss132 <valross1321@gmail.com>
2025-04-09 19:59:08 +0300

    setup terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,76 @@
+locals { 
+    bucket_name = "tf-devops-bucket"
+}
+
+// Create SA
+resource "yandex_iam_service_account" "sa" {
+  folder_id = local.folder_id
+  name      = "tf-test-sa"
+}
+
+// Grant permissions
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = local.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Create Static Access Keys
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Use keys to create bucket
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = local.bucket_name
+}
+
+resource "yandex_compute_instance" "docker_vm" {
+  name        = "docker-vm"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8vfvc6r3tl3k0igv6l"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.default.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+
+    # Скрипт установки Docker при создании ВМ
+    user-data = <<-EOT
+      #cloud-config
+      packages:
+        - docker.io
+      runcmd:
+        - systemctl enable docker
+        - systemctl start docker
+    EOT
+  }
+}
+
+resource "yandex_vpc_network" "default" {
+  name = "default-network"
+}
+
+resource "yandex_vpc_subnet" "default" {
+  name           = "default-subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.default.id
+  v4_cidr_blocks = ["10.0.0.0/24"]
+}
