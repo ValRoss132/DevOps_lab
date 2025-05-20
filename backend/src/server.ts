6e97a044d3ae45535c191e39f5f094a4b7b9a3c0
ValRoss132 <valross1321@gmail.com>
2025-05-20 12:48:10 +0300

    back metrics 2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
     next();
 });
 
-// метрики для Прометеуса
+// endpoint метрик
 app.get('/metrics', async (req, res) => {
     res.set('Content-Type', client.register.contentType);
     res.end(await client.register.metrics());
