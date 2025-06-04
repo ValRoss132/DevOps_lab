1271608dc7f949cf7dc987cec32b56fece5d1625
ValRoss132 <valross1321@gmail.com>
2025-06-04 15:06:37 +0300

    update db port

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,7 +6,7 @@ const config = {
     port: process.env.PORT || 3000,
     database: {
         host: process.env.DATABASE_HOST || 'localhost',
-        port: Number(process.env.DB_PORT) || 5432,
+        port: Number(process.env.DB_PORT) || 15432,
         name: process.env.DATABASE_NAME || 'database',
         user: process.env.DATABASE_USER || 'postgres',
         password: process.env.DATABASE_PASSWORD || 'password',
